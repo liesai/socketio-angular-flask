62b0b6a7e43fc2377e4d6fa5a790cf50c3780b7d
Marc Milbled <marc.milbled@gmail.com>
2023-01-28 17:14:12 -0500

    add amq function

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
+from threading import Thread
 from flask_socketio import SocketIO, send, emit
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pika
+import json
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -31,6 +34,42 @@ def amq():
     socketio.emit('topico', result, broadcast=True)
     return jsonify(result)
 
+
+def callback(ch, method, properties, body):
+    # votre logique de traitement du message ici
+    print("{X] AMQ ---- Received message: ", body)
+    data = []
+    time.sleep(2)
+    jsonToSend = json.loads(body.decode('utf-8'))
+    data.append(jsonToSend)
+    print(data)
+
+    socketio.emit('topico', data, broadcast=True)
+
+
+def start_consuming():
+    credentials = pika.PlainCredentials('python', 'G!tK_P_ZmB4qUUV')
+
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost', heartbeat=60, credentials=credentials))
+
+    except pika.AMQPConnectionError as e:
+        print(f'connexion failed : {e}')
+
+    channel = connection.channel()
+    channel.queue_declare(queue='toctoc')
+    channel.basic_consume(queue='toctoc', on_message_callback=callback)
+    print('Start consuming')
+    channel.start_consuming()
+
+
+def main():
+    consumer_thread = Thread(target=start_consuming)
+    consumer_thread.start()
+
+
+main()
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
---
+++ backend/make_load.py
@@ -0,0 +1,48 @@
+import requests
+import random
+import string
+import json
+from threading import Thread
+import pika
+
+
+x = 0
+letters = string.ascii_lowercase
+credentials = pika.PlainCredentials('python', 'G!tK_P_ZmB4qUUV')
+
+try:
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost', heartbeat=60, credentials=credentials))
+    print('connected')
+
+except pika.AMQPConnectionError as e:
+    print(f'connexion failed : {e}')
+
+
+def send_data_to_topic(topic, payload):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    if connection.is_open:
+        channel = connection.channel()
+        channel.exchange_declare(exchange=topic, exchange_type='topic')
+        channel.queue_bind(queue='toctoc', exchange=topic,
+                           routing_key='toctoc')
+        channel.basic_publish(exchange=topic, routing_key=topic, body=payload)
+        print(" [x] Sent %r:%r" % (topic, payload))
+        connection.close()
+    else:
+        print("Connection is closed, unable to send message.")
+
+
+while True:
+    topic = "toctoc"
+    url = "http://localhost:5000/amq"
+    payload = json.dumps({'offerId': x, 'status':
+                          ''.join(random.choice(letters) for i in range(10))})
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    x += 1
+    # r = requests.request('POST', url, headers=headers, data=payload)
+    # print(f'send data [x] #{x} {payload} - status: {r.status_code} ')
+    send_data_to_topic(topic, payload)
